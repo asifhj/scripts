5a0765e9f86aa5ebc41fa9968efe3709e23c87e4
Asif J <asif.h.j@gmail.com>
2014-11-25 20:33:29 +0530

    Get domain details

---
+++ get_domain_details.php
@@ -0,0 +1,571 @@
+<?php
+/*
+$info = "
+Whois Server Version 2.0
+
+Domain names in the .com and .net domains can now be registered
+with many different competing registrars. Go to http://www.internic.net
+for detailed information.
+
+   Domain Name: 5JCODELABS.COM
+   Registrar: PDR LTD. D/B/A PUBLICDOMAINREGISTRY.COM
+   Whois Server: whois.PublicDomainRegistry.com
+   Referral URL: http://www.PublicDomainRegistry.com
+   Name Server: NS1.HILLOCKHOSTING.COM
+   Name Server: NS2.HILLOCKHOSTING.COM
+   Status: ok
+   Updated Date: 20-sep-2013
+   Creation Date: 30-sep-2011
+   Expiration Date: 30-sep-2014
+
+>>> Last update of whois database: Mon, 20 Jan 2014 05:23:16 UTC <<<
+
+NOTICE: The expiration date displayed in this record is the date the 
+registrar's sponsorship of the domain name registration in the registry is 
+currently set to expire. This date does not necessarily reflect the expiration 
+date of the domain name registrant's agreement with the sponsoring 
+registrar.  Users may consult the sponsoring registrar's Whois database to 
+view the registrar's reported date of expiration for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois 
+database through the use of electronic processes that are high-volume and 
+automated except as reasonably necessary to register domain names or 
+modify existing registrations; the Data in VeriSign Global Registry 
+Services' (\"VeriSign\") Whois database is provided by VeriSign for 
+information purposes only, and to assist persons in obtaining information 
+about or related to a domain name registration record. VeriSign does not 
+guarantee its accuracy. By submitting a Whois query, you agree to abide 
+by the following terms of use: You agree that you may use this Data only 
+for lawful purposes and that under no circumstances will you use this Data 
+to: (1) allow, enable, or otherwise support the transmission of mass 
+unsolicited, commercial advertising or solicitations via e-mail, telephone, 
+or facsimile; or (2) enable high volume, automated, electronic processes 
+that apply to VeriSign (or its computer systems). The compilation, 
+repackaging, dissemination or other use of this Data is expressly 
+prohibited without the prior written consent of VeriSign. You agree not to 
+use electronic processes that are automated and high-volume to access or 
+query the Whois database except as reasonably necessary to register 
+domain names or modify existing registrations. VeriSign reserves the right 
+to restrict your access to the Whois database in its sole discretion to ensure 
+operational stability.  VeriSign may restrict or terminate your access to the 
+Whois database for failure to abide by these terms of use. VeriSign 
+reserves the right to modify these terms at any time. 
+
+The Registry database contains ONLY .COM, .NET, .EDU domains and
+Registrars.
+Domain Name: 5JCODELABS.COM
+Registry Domain ID: 1679745849_DOMAIN_COM-VRSN
+Registrar WHOIS Server: whois.publicdomainregistry.com
+Registrar URL: www.publicdomainregistry.com
+Updated Date: 20-Sep-2013
+Creation Date: 30-Sep-2011
+Registrar Registration Expiration Date: 30-Sep-2014
+Registrar: PDR Ltd. d/b/a PublicDomainRegistry.com
+Registrar IANA ID: 303
+Registrar Abuse Contact Email: abuse-contact@publicdomainregistry.com
+Registrar Abuse Contact Phone: +1-2013775952
+Domain Status: OK
+Registry Registrant ID: DI_29902286
+Registrant Name: Hillockhosting
+Registrant Organization: Hillockhosting
+Registrant Street: venkatesh nivas compound Karwar  
+Registrant City: Karwar
+Registrant State/Province: Karnataka
+Registrant Postal Code: 581301
+Registrant Country: IN
+Registrant Phone: +91.8553142384
+Registrant Phone Ext: 
+Registrant Fax: 
+Registrant Fax Ext: 
+Registrant Email: webmaster@hillockhosting.com
+Registry Admin ID: DI_29902286
+Admin Name: Hillockhosting
+Admin Organization: Hillockhosting
+Admin Street: venkatesh nivas compound Karwar 
+Admin City: Karwar
+Admin State/Province: Karnataka
+Admin Postal Code: 581301
+Admin Country: IN
+Admin Phone: +91.8553142384
+Admin Phone Ext: 
+Admin Fax: 
+Admin Fax Ext: 
+Admin Email: webmaster@hillockhosting.com
+Registry Tech ID: DI_29902286
+Tech Name: Hillockhosting
+Tech Organization: Hillockhosting
+Tech Street: venkatesh nivas compound Karwar 
+Tech City: Karwar
+Tech State/Province: Karnataka
+Tech Postal Code: 581301
+Tech Country: IN
+Tech Phone: +91.8553142384
+Tech Phone Ext: 
+Tech Fax: 
+Tech Fax Ext: 
+Tech Email: webmaster@hillockhosting.com
+Name Server: ns1.hillockhosting.com
+Name Server: ns2.hillockhosting.com
+
+DNSSEC:Unsigned
+
+Registration Service Provided By: CYBER DATA SOLUTION
+
+The data in this whois database is provided to you for information purposes 
+only, that is, to assist you in obtaining information about or related to a 
+domain name registration record. We make this information available \"as is\",
+and do not guarantee its accuracy. By submitting a whois query, you agree 
+that you will use this data only for lawful purposes and that, under no 
+circumstances will you use this data to: 
+(1) enable high volume, automated, electronic processes that stress or load 
+this whois database system providing you this information; or 
+(2) allow, enable, or otherwise support the transmission of mass unsolicited, 
+commercial advertising or solicitations via direct mail, electronic mail, or 
+by telephone. 
+The compilation, repackaging, dissemination or other use of this data is 
+expressly prohibited without prior written consent from us. The Registrar of 
+record is PDR Ltd. d/b/a PublicDomainRegistry.com. 
+We reserve the right to modify these terms at any time. 
+By submitting this query, you agree to abide by these terms.
+
+URL of the ICANN WHOIS Data Problem Reporting System:http://wdprs.internic.net/
+Last update of WHOIS database: Mon Jan 20 05:23:57 GMT 2014
+
+";
+
+$info2 = "#
+# ARIN WHOIS data and services are subject to the Terms of Use
+# available at: https://www.arin.net/whois_tou.html
+#
+
+
+#
+# The following results may also be obtained via:
+# http://whois.arin.net/rest/nets;q=152.2.181.221?showDetails=true&showARIN=false&ext=netref2
+#
+
+NetRange:       152.2.0.0 - 152.2.255.255
+CIDR:           152.2.0.0/16
+OriginAS:       
+NetName:        UNCCH-NET
+NetHandle:      NET-152-2-0-0-1
+Parent:         NET-152-0-0-0-0
+NetType:        Direct Assignment
+RegDate:        1991-06-07
+Updated:        2012-04-02
+Ref:            http://whois.arin.net/rest/net/NET-152-2-0-0-1
+
+OrgName:        University of North Carolina at Chapel Hill
+OrgId:          UNC-1-Z
+Address:        440 W. Franklin Street
+Address:        CB#1150
+City:           Chapel Hill
+StateProv:      NC
+PostalCode:     27599-1150
+Country:        US
+RegDate:        2010-01-06
+Updated:        2011-07-19
+Ref:            http://whois.arin.net/rest/org/UNC-1-Z
+
+OrgAbuseHandle: ISG10-ARIN
+OrgAbuseName:   IT Security Group
+OrgAbusePhone:  +1-919-445-9397 
+OrgAbuseEmail:  abuse@unc.edu
+OrgAbuseRef:    http://whois.arin.net/rest/poc/ISG10-ARIN
+
+OrgTechHandle: ZU52-ARIN
+OrgTechName:   University of North Carolina
+OrgTechPhone:  +1-919-962-6503 
+OrgTechEmail:  hostmaster@unc.edu
+OrgTechRef:    http://whois.arin.net/rest/poc/ZU52-ARIN
+
+RTechHandle: ZU52-ARIN
+RTechName:   University of North Carolina
+RTechPhone:  +1-919-962-6503 
+RTechEmail:  hostmaster@unc.edu
+RTechRef:    http://whois.arin.net/rest/poc/ZU52-ARIN
+
+
+#
+# ARIN WHOIS data and services are subject to the Terms of Use
+# available at: https://www.arin.net/whois_tou.html
+#
+";
+$all_domains = [];
+$all_unquie_domains = [];
+
+$all_domain_names = [];
+$all_unquie_domain_names = [];
+
+$all_domain_ip = [];
+$all_unquie_domain_ip = [];
+
+$input = fopen("host_names.csv","r");
+while(! feof($input))
+{
+	$domain = fgetcsv($input);
+	$domain = trim($domain[0]);
+	$d = explode(".", $domain);	
+	$dom = '';
+	array_push($all_domains, $domain);
+	$valid = preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $domain);
+	if(!$valid)
+	{
+		if(count($d)==1)
+		{
+			echo "$d[0]\n";
+			$dom = 1;
+		}
+		else
+		if(count($d)==2)
+		{
+			//echo "$d[0].$d[1]\t\t\t$domain\n";
+			$dom = "$d[0].$d[1]";
+		}
+		else
+		if(count($d)==3)		
+		{
+			if($d[1]=="com" || $d[1]=="co" || $d[1]=="nic" || $d[1]=="in" || $d[1]=="net")
+			{
+				//echo "$d[0].$d[1].$d[2]\t\t\t$domain\n";
+				$dom = "$d[0].$d[1].$d[2]";
+			}
+			else
+			{
+				//echo "$d[1].$d[2]\t\t\t$domain\n";
+				$dom = "$d[1].$d[2]";
+			}
+		}
+		else
+		if(count($d)==4)
+		{
+			if($d[2]=="com" || $d[2]=="co" || $d[2]=="nic" || $d[2]=="in" || $d[2]=="net")
+			{
+				//echo "$d[1].$d[2].$d[3]\t\t\t$domain\n";
+				$dom = "$d[1].$d[2].$d[3]";
+			}
+			else
+			{
+				//echo "$d[2].$d[3]\t\t\t$domain\n";
+				$dom = "$d[2].$d[3]";
+			}
+		}
+		else
+		if(count($d)==5)
+		{
+			if($d[3]=="com" || $d[3]=="co" || $d[3]=="nic" || $d[3]=="in" || $d[3]=="net")
+			{
+				//echo "$d[2].$d[3].$d[4]\t\t\t$domain\n";
+				$dom = "$d[2].$d[3].$d[4]";
+			}
+			else
+			{
+				//echo "$d[3].$d[4]\t\t\t$domain\n";
+				$dom = "$d[3].$d[4]";
+			}
+		}
+		else
+		if(count($d)==6)
+		{
+			if($d[4]=="com" || $d[4]=="co" || $d[4]=="nic" || $d[4]=="in" || $d[4]=="net")
+			{
+				//echo "$d[3].$d[4].$d[5]\t\t\t$domain\n";
+				$dom = "$d[3].$d[4].$d[5]";
+			}
+			else
+			{
+				//echo "$d[4].$d[5]\t\t\t$domain\n";
+				$dom = "$d[4].$d[5]";
+			}
+		}
+		array_push($all_domain_names, $dom);
+	}
+	else
+	{				
+		array_push($all_domain_ip, $domain);
+	}
+}
+
+$all_unique_domains = array_unique($all_domains);
+$all_unique_domain_names = array_unique($all_domain_names);
+$all_unique_domain_ip = array_unique($all_domain_ip);
+
+fclose($input);
+//var_dump($all_unquie_domains);
+//var_dump($all_domain_ip);
+echo count($all_unique_domain_names)."\t".count($all_domain_names)."\n";
+//var_dump($all_unique_domain_names);
+*/
+
+$input = fopen("nexeo_domains.csv","r");
+$domain_output = fopen("nexeo_domain_details.csv","w");
+$ip_output = fopen("nexeo_ip_details.csv","w");
+
+$header = "Given,Domain Name,Registry Domain ID,Registrar WHOIS Server,Registrar URL,Updated Date,Creation Date,Registrar Registration Expiration Date,Registrar,Registrar IANA ID,Registrar Abuse Contact Email,Registrar Abuse Contact Phone,Domain Status,Registry Registrant ID,Registrant Name,Registrant Organization,Registrant Street,Registrant City,Registrant State/Province,Registrant Postal Code,Registrant Country,Registrant Phone,Registrant Phone Ext,Registrant Fax,Registrant Fax Ext,Registrant Email,Registry Admin ID,Admin Name,Admin Organization,Admin Street,Admin City,Admin State/Province,Admin Postal Code,Admin Country,Admin Phone,Admin Phone Ext,Admin Fax,Admin Fax Ext,Admin Email,Registry Tech ID,Tech Name,Tech Organization,Tech Street,Tech City,Tech State/Province,Tech Postal Code,Tech Country,Tech Phone,Tech Phone Ext,Tech Fax,Tech Fax Ext,Tech Email,Name Server,Name Server,DNSSEC,Registration Service Provided By";
+fputcsv($domain_output,explode(',',$header));
+
+$header = "NetRange,CIDR,OriginAS,NetName,NetHandle,Parent,NetType,RegDate,Updated,Ref,OrgName,OrgId,Address,Address,City,StateProv,PostalCode,Country,RegDate,Updated,Ref,OrgAbuseHandle,OrgAbuseName,OrgAbusePhone,OrgAbuseEmail,OrgAbuseRef,OrgTechHandle,OrgTechName,OrgTechPhone,OrgTechEmail,OrgTechRef,RTechHandle,RTechName,RTechPhone,RTechEmail,RTechRef";
+fputcsv($ip_output,explode(',',$header));
+
+$counter = 0;
+
+foreach ($all_unique_domain_names as $key => $value) 
+{
+	$domain = $value;
+	$domain = trim($domain);
+	$d = explode(".", $domain);
+	$valid = preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $domain);
+	$dom = '';
+	$counter++;
+
+	if(!$valid)
+	{			
+		if(count($d)==1)
+		{
+			echo "$d[0]\n";
+			$dom = 1;
+		}
+		else
+		if(count($d)==2)
+		{
+			//echo "$d[0].$d[1]\t\t\t$domain\n";
+			$dom = "$d[0].$d[1]";
+		}
+		else
+		if(count($d)==3)		
+		{
+			if($d[1]=="com" || $d[1]=="co" || $d[1]=="nic" || $d[1]=="in" || $d[1]=="net")
+			{
+				//echo "$d[0].$d[1].$d[2]\t\t\t$domain\n";
+				$dom = "$d[0].$d[1].$d[2]";
+			}
+			else
+			{
+				//echo "$d[1].$d[2]\t\t\t$domain\n";
+				$dom = "$d[1].$d[2]";
+			}
+		}
+		else
+		if(count($d)==4)
+		{
+			if($d[2]=="com" || $d[2]=="co" || $d[2]=="nic" || $d[2]=="in" || $d[2]=="net")
+			{
+				//echo "$d[1].$d[2].$d[3]\t\t\t$domain\n";
+				$dom = "$d[1].$d[2].$d[3]";
+			}
+			else
+			{
+				//echo "$d[2].$d[3]\t\t\t$domain\n";
+				$dom = "$d[2].$d[3]";
+			}
+		}
+		else
+		if(count($d)==5)
+		{
+			if($d[3]=="com" || $d[3]=="co" || $d[3]=="nic" || $d[3]=="in" || $d[3]=="net")
+			{
+				//echo "$d[2].$d[3].$d[4]\t\t\t$domain\n";
+				$dom = "$d[2].$d[3].$d[4]";
+			}
+			else
+			{
+				//echo "$d[3].$d[4]\t\t\t$domain\n";
+				$dom = "$d[3].$d[4]";
+			}
+		}
+		else
+		if(count($d)==6)
+		{
+			if($d[4]=="com" || $d[4]=="co" || $d[4]=="nic" || $d[4]=="in" || $d[4]=="net")
+			{
+				//echo "$d[3].$d[4].$d[5]\t\t\t$domain\n";
+				$dom = "$d[3].$d[4].$d[5]";
+			}
+			else
+			{
+				//echo "$d[4].$d[5]\t\t\t$domain\n";
+				$dom = "$d[4].$d[5]";
+			}
+		}
+		//echo $dom."\n";
+		if($dom!=1)
+		{
+			echo "\n$counter,$dom,";
+			$info = shell_exec("whois $dom");			
+			$len = strlen($info);
+			$start_pos = strpos($info, "Creation Date:")+14;						
+			if($start_pos==14)
+			{
+				$start_pos = strpos($info, "Created On:")+11;
+				if($start_pos==11)						
+				{
+					$start_pos = strpos($info, "Domain Registration Date:")+25;
+					if($start_pos==25)						
+					{
+						$start_pos = strpos($info, "created:")+8;
+						if($start_pos==8)						
+						{
+							$start_pos = strpos($info, "Domain record activated:")+24;
+						}						
+					}
+				}
+			}
+			$end_pos = strpos(substr($info, $start_pos), "\n");
+			
+			$info = substr($info, $start_pos, $end_pos);
+			
+			echo $info;
+
+			$row=[];
+			//$counter++;
+			array_push($row, $domain);			
+			$info=trim($info);
+			if(is_numeric($info[0]))
+			{
+				array_push($row, trim($info));
+				fputcsv($domain_output,$row);
+			}
+			else
+			{
+				array_push($row, "NF");
+				fputcsv($domain_output,$row);
+			}
+		}
+	}
+	else
+	{				
+		$ip_length = strlen($info2);
+		$ip_start_pos = strpos($info2, "NetRange:");		
+		$ip_end_pos = strpos($info2, "#
+		# ARIN");
+		$ip_end_len = strlen("The data in this whois database is provided to you for information purposes");
+
+		$info2 = substr($info, $ip_start_pos, $ip_end_pos-$ip_start_pos-1);
+		$infos2 = explode("\t\n", $info2);
+		$row=[];
+		array_push($row, $domain);
+		for($i=0;$i<count($infos2);$i++) 
+		{
+		   	
+			$value = trim($infos2[$i]);
+			if(strcmp($value, "")!=0)
+			{
+				$values = explode(":", $value);
+		 		array_push($row, $values[1]);
+		 	}
+		}
+		fputcsv($ip_output,$row);
+	}
+}
+fclose($input);
+fclose($domain_output);
+fclose($ip_output);
+
+
+$final = fopen("nexeo_final.csv","w");
+$header = "Input_Domain, Domain, Created_Date";
+fputcsv($final,explode(',',$header));
+
+$input = fopen("nexeo_host_names.csv","r");
+
+while(! feof($input))
+{
+	$row = fgetcsv($input);
+	$domain = $row[0];
+	//echo $domain;
+	$domain_inputs = fopen("domain_details2.csv","r");
+	$res='';
+	while(! feof($domain_inputs))
+	{
+		$row1 = fgetcsv($domain_inputs);
+		$d = $row1[0];
+		$res = endsWith($domain, $d);
+		$r=[];
+		if($res)
+		{
+			array_push($r, trim($domain));
+			array_push($r, trim($d));
+			array_push($r, trim($row1[1]));
+			fputcsv($final, $r);
+			echo "True==> ".$domain." = ".$d."\n";
+			break;
+		}
+	}
+	if(!$res)
+	{		
+		fputcsv($domain_output,$row);	
+	}
+	fclose($domain_inputs);
+}
+
+function endsWith($haystack, $needle)
+{
+    return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+}
+
+/*
+	$domain_inputs = fopen("domain_details.csv","r");
+	$domain_output = fopen("domain_details2.csv","w");
+	$header = "Input_Domain, Domain, Created_Date";
+	fputcsv($domain_output,explode(',',$header));
+
+	while(! feof($domain_inputs))
+	{
+		$r = fgetcsv($domain_inputs);
+		$domain = $r[0];
+		$date = $r[1];
+
+		if(strcmp(trim($date), "NF")==0)
+		{		
+			echo "$domain\t\t$date";	
+			$info = shell_exec("whois $domain");			
+			$len = strlen($info);
+			$start_pos = strpos($info, "Creation Date:")+14;						
+			if($start_pos==14)
+			{
+				$start_pos = strpos($info, "Created On:")+11;
+				if($start_pos==11)						
+				{
+					$start_pos = strpos($info, "Domain Registration Date:")+25;
+					if($start_pos==25)						
+					{
+						$start_pos = strpos($info, "created:")+8;
+						if($start_pos==8)						
+						{
+							$start_pos = strpos($info, "Domain record activated:")+24;							
+							if($start_pos==24)						
+							{
+								$start_pos = strpos($info, "Registered on:")+14;
+							}
+						}						
+					}
+				}
+			}
+			$end_pos = strpos(substr($info, $start_pos), "\n");			
+			$info = substr($info, $start_pos, $end_pos);			
+			echo "\t\t".$info."\n";
+			$row=[];			
+			array_push($row, $domain);			
+			$info=trim($info);
+			if(is_numeric($info[0]))
+			{
+				array_push($row, trim($info));
+				fputcsv($domain_output,$row);
+			}
+			else
+			{
+				array_push($row, "NF");
+				fputcsv($domain_output,$row);
+			}
+		}
+		else
+		{
+			$row = [];
+			array_push($row, $domain);
+			array_push($row, $date);
+			fputcsv($domain_output,$row);
+		}
+	}
+	fclose($domain_inputs);
+	fclose($domain_output);*/
+?>
